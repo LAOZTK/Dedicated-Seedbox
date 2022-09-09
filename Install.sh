22e0c8c685a0185d0a35f69848791e043dab5e22
LAOZTK <38147713+LAOZTK@users.noreply.github.com>
2022-09-09 16:20:25 +0800

    Called  Check Linux Distro

--- Install.sh
+++ Install.sh
@@ -12,11 +12,11 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 ## Check Linux Distro
-distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
-if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
-	warn_1; echo "Only Debian 10/11 is supported"; normal_4
-	exit 1
-fi
+#distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
+#if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
+#	warn_1; echo "Only Debian 10/11 is supported"; normal_4
+	#exit 1
+#fi
 
 ## Check Virtual Environment
 systemd-detect-virt > /dev/null
